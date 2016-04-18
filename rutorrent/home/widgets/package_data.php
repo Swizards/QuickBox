ca514451cef7e42a4a9f03dc84f450400271fcfe
JMSDOnline <jmsolodesigns@gmail.com>
2016-04-18 10:34:57 -0500

    Updated System Response Msg on Plex and BTSync installer/uninstaller

--- rutorrent/home/widgets/package_data.php
+++ rutorrent/home/widgets/package_data.php
@@ -1,9 +1,9 @@
 <?php
 if ($_GET['installpackage-plex']) {
         //header('Location: /');
-        header('Refresh: 10; /');
-        writeMsg("Hello <b>$username</b>: I am completing the installation of the <b>Plex Media Server</b> package. Your browser will refresh once more when the installation has fully completed ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the installation of the <b>Plex Media Server</b> package. Your browser will refresh once more when the installation has fully completed ... <br>";
+        header('Refresh: 30; /');
+        writeMsg("Hello <b>$username</b>: I am completing the installation of the <b>Plex Media Server</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 30 seconds ... <br>");
+        $message = "Hello <b>$username</b>: I am completing the installation of the <b>Plex Media Server</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 30 seconds ... <br>";
         shell_exec("sudo /usr/bin/installpackage-plex");
         $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
         $myUrl .= '://'.$_SERVER['HTTP_HOST'];
@@ -11,9 +11,9 @@ if ($_GET['installpackage-plex']) {
 }
 if ($_GET['removepackage-plex']) {
         //header('Location: /');
-        header('Refresh: 10; /');
-        writeMsg("Hello <b>$username</b>: I am completing the removal of the <b>Plex Media Server</b> package. Your browser will refresh once more when the uninstall has fully completed ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the removal of the <b>Plex Media Server</b> package. Your browser will refresh once more when the uninstall has fully completed ... <br>";
+        header('Refresh: 30; /');
+        writeMsg("Hello <b>$username</b>: I am completing the removal of the <b>Plex Media Server</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 30 seconds ... <br>");
+        $message = "Hello <b>$username</b>: I am completing the removal of the <b>Plex Media Server</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 30 seconds ... <br>";
         shell_exec("sudo /usr/bin/removepackage-plex");
         $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
         $myUrl .= '://'.$_SERVER['HTTP_HOST'];
@@ -21,9 +21,9 @@ if ($_GET['removepackage-plex']) {
 }
 if ($_GET['installpackage-btsync']) {
         //header('Location: /');
-        header('Refresh: 10; /');
-        writeMsg("Hello <b>$username</b>: I am completing the installation of the <b>BTSync</b> package. Your browser will refresh once more when the installation has fully completed ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the installation of the <b>BTSync</b> package. Your browser will refresh once more when the installation has fully completed ... <br><br>";
+        header('Refresh: 30; /');
+        writeMsg("Hello <b>$username</b>: I am completing the installation of the <b>BTSync</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 30 seconds ... <br>");
+        $message = "Hello <b>$username</b>: I am completing the installation of the <b>BTSync</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 30 seconds ... <br><br>";
         shell_exec("sudo /usr/bin/installpackage-btsync");
         $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
         $myUrl .= '://'.$_SERVER['HTTP_HOST'];
@@ -31,9 +31,9 @@ if ($_GET['installpackage-btsync']) {
 }
 if ($_GET['removepackage-btsync']) {
         //header('Location: /');
-        header('Refresh: 10; /');
-        writeMsg("Hello <b>$username</b>: I am completing the removal of the <b>BTSync</b> package. Your browser will refresh once more when the uninstall has fully completed ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the removal of the <b>BTSync</b> package. Your browser will refresh once more when the uninstall has fully completed ... <br>";
+        header('Refresh: 30; /');
+        writeMsg("Hello <b>$username</b>: I am completing the removal of the <b>BTSync</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 30 seconds ... <br>");
+        $message = "Hello <b>$username</b>: I am completing the removal of the <b>BTSync</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 30 seconds ... <br>";
         shell_exec("sudo /usr/bin/removepackage-btsync");
         $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
         $myUrl .= '://'.$_SERVER['HTTP_HOST'];
