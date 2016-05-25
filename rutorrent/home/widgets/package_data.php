fe36c40ba373f25b0aa54862c0cd5e09030bba37
JMSDOnline <jmsolodesigns@gmail.com>
2016-05-25 17:02:04 -0500

    [enhancement] CSF added to system/package/commands on dashboard

--- rutorrent/home/widgets/package_data.php
+++ rutorrent/home/widgets/package_data.php
@@ -1,24 +1,5 @@
 <?php
-if ($_GET['installpackage-plex']) {
-        //header('Location: /');
-        header('Refresh: 15; /');
-        writeMsg("Hello <b>$username</b>: I am completing the installation of the <b>Plex Media Server</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the installation of the <b>Plex Media Server</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br>";
-        shell_exec("sudo /usr/local/bin/installpackage-plex");
-        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
-        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
-        $newURL = $myURL;
-}
-if ($_GET['removepackage-plex']) {
-        //header('Location: /');
-        header('Refresh: 15; /');
-        writeMsg("Hello <b>$username</b>: I am completing the removal of the <b>Plex Media Server</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>");
-        $message = "Hello <b>$username</b>: I am completing the removal of the <b>Plex Media Server</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>";
-        shell_exec("sudo /usr/local/bin/removepackage-plex");
-        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
-        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
-        $newURL = $myURL;
-}
+
 if ($_GET['installpackage-btsync']) {
         //header('Location: /');
         header('Refresh: 15; /');
@@ -39,4 +20,47 @@ if ($_GET['removepackage-btsync']) {
         $myUrl .= '://'.$_SERVER['HTTP_HOST'];
         $newURL = $myURL;
 }
+
+if ($_GET['installpackage-csf']) {
+        //header('Location: /');
+        header('Refresh: 15; /');
+        writeMsg("Hello <b>$username</b>: I am completing the installation of the <b>Config Server Firewall (CSF)</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br>");
+        $message = "Hello <b>$username</b>: I am completing the installation of the <b>Config Server Firewall (CSF)</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br><br>";
+        shell_exec("sudo /usr/local/bin/installpackage-csf");
+        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
+        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
+        $newURL = $myURL;
+}
+if ($_GET['removepackage-csf']) {
+        //header('Location: /');
+        header('Refresh: 15; /');
+        writeMsg("Hello <b>$username</b>: I am completing the removal of the <b>Config Server Firewall (CSF)</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>");
+        $message = "Hello <b>$username</b>: I am completing the removal of the <b>Config Server Firewall (CSF)</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>";
+        shell_exec("sudo /usr/local/bin/removepackage-csf");
+        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
+        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
+        $newURL = $myURL;
+}
+
+if ($_GET['installpackage-plex']) {
+        //header('Location: /');
+        header('Refresh: 15; /');
+        writeMsg("Hello <b>$username</b>: I am completing the installation of the <b>Plex Media Server</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br>");
+        $message = "Hello <b>$username</b>: I am completing the installation of the <b>Plex Media Server</b> package. Your browser will refresh once more when the installation has fully completed, this may take upwards of 15 seconds ... <br>";
+        shell_exec("sudo /usr/local/bin/installpackage-plex");
+        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
+        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
+        $newURL = $myURL;
+}
+if ($_GET['removepackage-plex']) {
+        //header('Location: /');
+        header('Refresh: 15; /');
+        writeMsg("Hello <b>$username</b>: I am completing the removal of the <b>Plex Media Server</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>");
+        $message = "Hello <b>$username</b>: I am completing the removal of the <b>Plex Media Server</b> package. Your browser will refresh once more when the uninstall has fully completed, this may take upwards of 15 seconds ... <br>";
+        shell_exec("sudo /usr/local/bin/removepackage-plex");
+        $myUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && !in_array(strtolower($_SERVER['HTTPS']),array('off','no'))) ? 'https' : 'http';
+        $myUrl .= '://'.$_SERVER['HTTP_HOST'];
+        $newURL = $myURL;
+}
+
 ?>
